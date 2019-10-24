945290e759f7b92bac4949a529b0afbc54777ad4
Michael Kovner <mkovner@tableau.com>
2019-10-24 15:05:48 -0700

    build to docs

---
+++ docs/precache-manifest.ea1455fb80fa8242dc64d5759aaed135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0aaf88f3a688a82c75d6f65a276c790",
+    "url": "/tableau-emailer-extension/index.html"
+  },
+  {
+    "revision": "dd0bcc211e175dc3eeb5",
+    "url": "/tableau-emailer-extension/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5338f1d5f3bbe5d6445a",
+    "url": "/tableau-emailer-extension/static/css/main.5dce65d7.chunk.css"
+  },
+  {
+    "revision": "dd0bcc211e175dc3eeb5",
+    "url": "/tableau-emailer-extension/static/js/2.68b66770.chunk.js"
+  },
+  {
+    "revision": "5338f1d5f3bbe5d6445a",
+    "url": "/tableau-emailer-extension/static/js/main.bae19af8.chunk.js"
+  },
+  {
+    "revision": "7ce6e17dd7042c9872cf",
+    "url": "/tableau-emailer-extension/static/js/runtime-main.15ed3eba.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tableau-emailer-extension/precache-manifest.ea1455fb80fa8242dc64d5759aaed135.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tableau-emailer-extension/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
